88f8343931eed476e071af99e74009e1683c0f23
yupaits <495606653@qq.com>
2017-08-05 18:35:56 +0800

    弃用 SpringCloud 微服务架构搭建文档管理系统服务；使用JWT做认证和授权；

--- src/main/java/com/yupaits/docsservice/DocsServiceApplication.java
+++ src/main/java/com/yupaits/docsservice/DocsServiceApplication.java
@@ -2,10 +2,7 @@ package com.yupaits.docsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class DocsServiceApplication {
 
---
+++ src/main/java/com/yupaits/docsservice/security/JwtTokenService.java
@@ -0,0 +1,14 @@
+package com.yupaits.docsservice.security;
+
+
+import org.springframework.security.core.Authentication;
+
+/**
+ * Created by yupaits on 2017/8/5.
+ */
+public interface JwtTokenService {
+
+    String createJwtToken(Authentication auth, int minutes);
+
+    Authentication parseJwtToken(String jwtToken);
+}
