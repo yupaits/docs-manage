fef6d039042308ec8aea19c4ea5cf0e5f83fc2c9
Gogs <gogs@fake.local>
2018-08-17 00:15:21 +0800

    开发页面

--- docs-ui/src/App.vue
+++ docs-ui/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <a-locale-provider :locale="locale">
+    <div id="app">
+      <router-view/>
+    </div>
+  </a-locale-provider>
 </template>
 
 <script>
+  import zhCN from 'vue-antd-ui/lib/locale-provider/zh_CN'
   export default {
-    name: 'app'
+    data() {
+      return {
+        locale: zhCN
+      }
+    }
   }
 </script>
 
+
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif, "Microsoft YaHei UI";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
 </style>
